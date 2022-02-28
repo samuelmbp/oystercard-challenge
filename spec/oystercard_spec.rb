bcd77da1b3afad50946765278b9fc705fcd388d6
ruiined <ruiined@gmail.com>
2022-02-28 14:33:24 +0000

    created a test to check the default balance value
    Co-authored-by: Samuel Raducan <samuelmbp@users.noreply.github.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,16 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  it "returns an instance of the class with a perameter" do
+    oystercard = Oystercard.new(10)
+    expect(oystercard).to be_an_instance_of(Oystercard)
+  end
+
+  it 'has a balance of 0 by default' do
+    expect(subject.balance).to eq 0
+  end
+
+
 end
+
